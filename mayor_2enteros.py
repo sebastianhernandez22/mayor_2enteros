9ce868aa14c2419498fadb0344e5bfd6fe0aea80
sebastianhernandez22 <sebastianhernandez@colegioguanenta.edu.co>
2023-02-23 16:07:26 -0500

    Se agrego el programa

--- mayor_2enteros.py
+++ mayor_2enteros.py
@@ -1,14 +1,26 @@
 # prgrama para verificar cual de dos numeros enteros es el mayor
 
 print("----------------------------------------")
-print("----------------------------------------")
+print("-------------ENTERO MAYOR---------------")
 print("----------------------------------------")
 
 # input
+X = int(input("Digite el valor de x: "))
+y = int(input("Digite el valor de y:"))
 
 # processing
+if X == y:
+    print("Los numeros son iguales...")
 
+else:
+    if X > y:
+        mayor = X
+    else:
+        mayor = y
+        print("El mayor entre" + str(X) + " y " + str(y) + " es " + str(mayor))    
+ 
 #output
 print("----------------------------------------")
+print("--------------RESULTADOS----------------")
 print("----------------------------------------")
-print("----------------------------------------")
+print("EL MAYOR ENTRE: " + str(X) + " y " + str(y) + " es " + str(mayor))
